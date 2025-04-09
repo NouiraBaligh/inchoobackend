1ba40d56a137661a5dba2de8a754c1c4506281d8
Nouira Baligh <nouirabaligh009@gmail.com>
2025-04-09 15:33:41 +0100

    add nodemailer topic

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,16 +1,54 @@
 import Order from "../models/order.models.js";
 import asyncHandler from "express-async-handler";
+import nodemailer from "nodemailer";
 
 // CREATE ORDER
 const createOrder = asyncHandler(async (req, res) => {
   const newOrder = Order(req.body);
   const savedOrder = await newOrder.save();
+
   if (!savedOrder) {
     res.status(400);
     throw new Error("Order was not created");
-  } else {
-    res.status(201).json(savedOrder);
   }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.MAIL_USER,
+    to: "nouirabaligh009@gmail.com",
+    subject: "🛒 New Order Received From Inchoo Store",
+    html: `
+      <h2>Inchoo Store : New Order Created</h2>
+      <p><strong>Order ID:</strong> ${savedOrder._id}</p>
+      <p><strong>Customer:</strong> ${savedOrder.prename || ""} ${
+      savedOrder.name || ""
+    }</p>
+      <p><strong>Total:</strong> ${savedOrder.totalPrice || "N/A"} TND</p>
+      <p><strong>Date:</strong> ${new Date(
+        savedOrder.createdAt
+      ).toLocaleString()}</p>
+      
+      <hr/>
+      <p>This is an automatic email from your e-commerce platform Inchoo Store.</p>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Failed to send email:", error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  res.status(201).json(savedOrder);
 });
 
 // UPDATE ORDER
